0c6a00379500cdf43905b0951b12393819689b07
PRIYANSHU KUMAR <113517876+pk8789764353@users.noreply.github.com>
2025-04-18 20:54:40 +0530

    gg

---
+++ app.js
@@ -0,0 +1,55 @@
+const apikey = "d5079086f18414d0463a4eb1f6d78038";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".weather").style.display = "none";
+        document.querySelector(".error").style.display = "block";
+    } else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "clouds.png";
+
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "clear.png";
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "rain.png";
+        }
+
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "drizzle.png";
+        }
+
+        else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "mist.png";
+        }
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+
+    }
+
+
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+
+})
+searchBox.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        checkWeather(searchBox.value);
+    }
+});
